ecb7690db0f5d5b2aa871b1eeb8088aa2ded5052
James Hurford <terrasea@gmail.com>
2017-01-23 00:34:52 +0000

    Implemented the API for search and getting of articles
    Included a fix to get json output to work

--- bin/server.py
+++ bin/server.py
@@ -1,7 +1,31 @@
 import cherrypy
+from json import JSONEncoder
+import requests
+
+import rss_reader
 
 class Root(object):
-    pass
+    @cherrypy.tools.json_out()
+    @cherrypy.expose
+    def article(self, guid):
+        response = requests.get('http://feeds.bbci.co.uk/news/world/rss.xml')
+
+        rss_feed_dict = rss_reader.rss_to_dict(str(response.content, 'utf-8'))
+
+        return rss_reader.get_article(guid, rss_feed_dict)
+
+
+    @cherrypy.tools.json_out()
+    @cherrypy.expose
+    def articles(self, keywords=None):
+        response = requests.get('http://feeds.bbci.co.uk/news/world/rss.xml')
+   
+        rss_feed_dict = rss_reader.rss_to_dict(str(response.content, 'utf-8'))
+        if keywords and keywords.split():
+            return list(rss_reader.search_by_keywords(keywords.split(), rss_feed_dict))
+
+        return rss_feed_dict.get('items', [])
+
 
 if __name__ == '__main__':
     cherrypy.tree.mount(Root(), '/', 'app.conf')
